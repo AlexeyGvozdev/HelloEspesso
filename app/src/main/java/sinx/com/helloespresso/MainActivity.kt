4d58c864ce5ec0bfa176b0b01387422e3b1fbc2b
Alexey Gvozdev <lesha_chess@mail.ru>
2019-05-11 17:56:52 +0400

    init

---
+++ app/src/main/java/sinx/com/helloespresso/MainActivity.kt
@@ -0,0 +1,12 @@
+package sinx.com.helloespresso
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
